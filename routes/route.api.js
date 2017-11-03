a99f9e8223f415784fe3a363f8ba38cd5190330c
myemail <myemail@emal.com>
2017-11-03 18:57:12 +0800

    提交代码

--- routes/route.api.js
+++ routes/route.api.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
+/** 获取前10个数据 */
+router.get('/top10',function (req,res,next) {
+    res.json({
+       items:['page1','page2','page3','page4','page5','page6','page7','page8','page9','page10']
+    });
+});
 
+/** 获取分类 */
+router.get('/top10',function (req,res,next) {
+    res.json({
+       items:['page1','page2','page3','page4','page5','page6','page7','page8','page9','page10']
+    });
+});
 
 module.exports = router;
--- routes/route.page.js
+++ routes/route.page.js
@@ -11,4 +11,11 @@ router.get("/users", function(req, res, next) {
   res.send("respond with a resource");
 });
 
+/* GET posts page. */
+router.get("/posts", function(req, res, next) {
+  res.render("posts", { title: "Express" });
+});
+
+
+
 module.exports = router;
