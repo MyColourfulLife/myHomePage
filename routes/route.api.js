e24f647c2fd4b18a984a77de76d64e4d81bf7bf9
huangjiashu <huangjiashu@niubi.com>
2017-11-22 15:19:03 +0800

    统一错误处理

--- routes/route.api.js
+++ routes/route.api.js
@@ -2,41 +2,37 @@ var express = require("express");
 var router = express.Router();
 var CategoryModel = require("../models/category");
 var PostModel = require("../models/post");
+var errorHandle = require('../common/errorHandle');
 
 /** 获取前10个数据 */
-router.get("/top10", function(req, res, next) {
+router.get("/top10", function (req, res, next) {
   let numbers = req.query.numbers || 10;
 
-  PostModel.find({},function (err,docs) {
+  PostModel.find({}, function (err, docs) {
 
     if (err) {
-        res.json({
-            sucess:false,
-            reason:err.message
-        })
-        return;
+      errorHandle(err, next);
+    } else {
+      var docs = docs.reverse();
+      if (docs.length > numbers) {
+        docs = docs.slice(0, 10);
+      }
+      res.json({
+        sucess: true,
+        items: docs
+      });
     }
 
-         var docs = docs.reverse();
-          if (docs.length > numbers) {
-                docs = docs.slice(0,10);
-          }
-          res.json({
-              sucess:true,
-            items:docs
-        });
-      });
+
+  });
 });
 
 /** 获取分类*/
-router.get("/posts/categories", function(req, res, next) {
-  CategoryModel.find({}, function(err, categories) {
+router.get("/posts/categories", function (req, res, next) {
+  CategoryModel.find({}, function (err, categories) {
 
     if (err) {
-      res.json({
-        sucess: false,
-        errmessage: err.message
-      });
+      errorHandle(err, next);
     } else {
       res.json({
         sucess: true,
@@ -47,82 +43,69 @@ router.get("/posts/categories", function(req, res, next) {
 });
 
 /** 获取单个分类的数据*/
-router.get('/posts/category',function (req,res,next) {
-    let id = ' ' + req.query.id + ' ';
-    PostModel.find({categoryId:id},function(err,articleLists) {
-      if (err) {
-        res.json({
-          sucess:false,
-          reason:err.message
-        });
-      }else{
-        res.json({
-            sucess:true,
-            postLists:articleLists.reverse()
-        });
-      }
+router.get('/posts/category', function (req, res, next) {
+  let id = ' ' + req.query.id + ' ';
+  PostModel.find({ categoryId: id }, function (err, articleLists) {
+    if (err) {
+      errorHandle(err, next);
+    } else {
+      res.json({
+        sucess: true,
+        postLists: articleLists.reverse()
+      });
+    }
 
-    });
+  });
 });
 
 /** 发布文章*/
-router.post('/v1/posts',function (req,res,next) { 
-   var title= req.body.title;
-   var content= req.body.content;
-   var categoryId=req.body.categoryId;
-   console.log(title,content,categoryId);
-   var post = new PostModel();
-   post.title = title;
-   post.content = content;
-   post.categoryId = categoryId;
+router.post('/posts', function (req, res, next) {
+  var title = req.body.title;
+  var content = req.body.content;
+  var categoryId = req.body.categoryId;
+  console.log(title, content, categoryId);
+  var post = new PostModel();
+  post.title = title;
+  post.content = content;
+  post.categoryId = categoryId;
 
-   post.save(function (err,doc) {
-       if (err) {
-           console.log(res);
-           res.json({
-               sucess:false,
-               reason:err.message
-           });
-       } else {
-           res.json({
-               sucess:true,
-               post:doc
-           });
-       }
-   });
+  post.save(function (err, doc) {
+    if (err) {
+      errorHandle(err, next);
+    } else {
+      res.json({
+        sucess: true,
+        post: doc
+      });
+    }
+  });
 });
 
 /** 修改文章*/
-router.patch('/v1/posts/:id',function (req,res,next) {
-    var id = req.params.id;
-    var title = req.body.title;
-    var content = req.body.content;
+router.patch('/posts/:id', function (req, res, next) {
+  var id = req.params.id;
+  var title = req.body.title;
+  var content = req.body.content;
 
-    PostModel.findOneAndUpdate({ _id: id }, { title, content }, function(err) {
-        if (err) {
-          res.json({
-              sucess:false,
-              reason:err.message
-          });
-        } else {
-          res.json({});
-        }
-      });
+  PostModel.findOneAndUpdate({ _id: id }, { title, content }, function (err) {
+    if (err) {
+      errorHandle(err, next);
+    } else {
+      res.json({});
+    }
+  });
 });
 
 /** 获取文章数据*/
-router.get('/v1/posts/:id',function (req,res,next) {
-    var id = req.params.id;
-    PostModel.findOne({_id:id},function (err,post,next) {
-        if (err) {
-            res.json({
-                sucess:false,
-                reason:err.message
-            });
-          } else {
-            res.json({ post });
-          }
-    });
+router.get('/posts/:id', function (req, res, next) {
+  var id = req.params.id;
+  PostModel.findOne({ _id: id }, function (err, post, next) {
+    if (err) {
+      errorHandle(err, next);
+    } else {
+      res.json({ post });
+    }
+  });
 });
 
 
